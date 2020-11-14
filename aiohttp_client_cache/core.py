3fc4508bae5febfe2f201bf6500c16d4d46dabec
Jordan Cook <jordan.cook@pioneer.com>
2020-11-14 14:43:33 -0600

    Move cache expiration logic from CachedSession to BaseCache; add more type annotations, Google-style docstrings

--- aiohttp_client_cache/core.py
+++ aiohttp_client_cache/core.py
@@ -1,7 +1,7 @@
 """Core functions for cache configuration"""
 from contextlib import contextmanager
 from datetime import datetime, timedelta
-from typing import Callable
+from typing import Callable, Union
 
 from aiohttp import ClientSession as OriginalSession
 from aiohttp_client_cache import backends
@@ -14,48 +14,41 @@ class CachedSession(OriginalSession):
         self,
         cache_name: str = 'cache',
         backend: str = None,
-        expire_after: int = None,
+        expire_after: Union[int, timedelta] = None,
         allowable_codes: tuple = (200,),
         allowable_methods: tuple = ('GET',),
         filter_fn: Callable = lambda r: True,
         **backend_options,
     ):
         """
-        :param cache_name: for ``sqlite`` backend: cache file will start with this prefix,
-                           e.g ``cache.sqlite``
+        Args:
+            cache_name: Cache prefix or namespace, depending on backend; see notes below
+            backend: cache backend name; see see :ref:`persistence` for details. May also be a
+                backend implementation subclassing :py:class:`.BaseCache`. Defaults to ``sqlite``
+                if available, otherwise fallback to ``memory``
+            expire_after: Number of seconds after cache will be expired, or ``None`` to never expire
+            allowable_codes: Limit caching only for response with this codes
+            allowable_methods: Cache only requests of this methods
+            filter_fn: function that takes a :py:class:`aiohttp.ClientResponse` object and
+                returns a boolean indicating whether or not that response should be cached. Will be
+                applied to both new and previously cached responses
+            include_get_headers: Make response headers part of the cache key
+            ignored_parameters: List of request parameters to be excluded from the cache key.
+            backend_options: Additional backend-specific options; see :py:module:`.backends` for details
 
-                           for ``mongodb``: it's used as database name
+        The ``cache_name`` parameter will be used as follows depending on the backend:
 
-                           for ``redis``: it's used as the namespace. This means all keys
-                           are prefixed with ``'cache_name:'``
-        :param backend: cache backend name e.g ``'sqlite'``, ``'mongodb'``, ``'redis'``, ``'memory'``.
-                        (see :ref:`persistence`). Or instance of backend implementation.
-                        Default value is ``None``, which means use ``'sqlite'`` if available,
-                        otherwise fallback to ``'memory'``.
-        :param expire_after: ``timedelta`` or number of seconds after cache will be expired
-                             or `None` (default) to ignore expiration
-        :param allowable_codes: limit caching only for response with this codes (default: 200)
-        :param allowable_methods: cache only requests of this methods (default: 'GET')
-        :param filter_fn: function to apply to each response; the response is only cached if
-                          this returns `True`. Note that this function does not not modify
-                          the cached response in any way.
-        :kwarg backend_options: options for chosen backend. See corresponding
-                                :ref:`sqlite <backends_sqlite>`, :ref:`mongo <backends_mongo>`
-                                and :ref:`redis <backends_redis>` backends API documentation
-        :param include_get_headers: If `True` headers will be part of cache key.
-                                    E.g. after get('some_link', headers={'Accept':'application/json'})
-                                    get('some_link', headers={'Accept':'application/xml'}) is not from cache.
-        :param ignored_parameters: List of parameters to be excluded from the cache key.
-                                   Useful when requesting the same resource through different
-                                   credentials or access tokens, passed as parameters.
+            * ``sqlite``: Cache filename prefix, e.g ``my_cache.sqlite``
+            * ``mongodb``: Database name
+            * ``redis``: Namespace, meaning all keys will be prefixed with ``'cache_name:'``
+
+        Note on cache key parameters: Set ``include_get_headers=False`` if you want responses to be
+        cached under the same key if they only differ by headers. You may also provide
+        ``ignored_parameters`` to ignore specific request params. This is useful, for example, when
+        requesting the same resource with different credentials or access tokens.
         """
-        self.cache = backends.create_backend(backend, cache_name, **backend_options)
+        self.cache = backends.create_backend(backend, cache_name, expire_after, **backend_options)
         self._cache_name = cache_name
-
-        if expire_after is not None and not isinstance(expire_after, timedelta):
-            expire_after = timedelta(seconds=expire_after)
-        self._cache_expire_after = expire_after
-
         self._cache_allowable_codes = allowable_codes
         self._cache_allowable_methods = allowable_methods
         self._filter_fn = filter_fn
@@ -68,12 +61,12 @@ class CachedSession(OriginalSession):
     async def request(self, method, url, **kwargs):
         cache_key = self.cache.create_key(method, url, **kwargs)
 
-        # Attempt to fetch cached response; if expired, delete it and fetch new one
-        response, timestamp = self.cache.get_response_and_time(cache_key)
-        if response is None or self._is_expired(timestamp):
-            self.cache.delete(cache_key)
+        # Attempt to fetch cached response; if missing or expired, fetch new one
+        response, timestamp = self.cache.get_response(cache_key)
+        if response is None or response.is_expired:
             return await self.send_and_cache_request(method, url, cache_key, **kwargs)
 
+        # TODO: Handle this in BaseCache
         # If the request is filtered out and has a previously cached response, delete it
         if getattr(response, "from_cache", False) and not self._filter_fn(response):
             self.cache.delete(cache_key)
@@ -94,19 +87,17 @@ class CachedSession(OriginalSession):
         response.from_cache = False
         return response
 
-    def _is_expired(self, timestamp):
-        time_elapsed = datetime.utcnow() - timestamp
-        return self._cache_expire_after and time_elapsed >= self._cache_expire_after
-
     @contextmanager
     def cache_disabled(self):
         """
-        Context manager for temporary disabling cache
-        ::
+        Context manager for temporarily disabling cache
+
+        Example:
 
             >>> s = CachedSession()
             >>> with s.cache_disabled():
             ...     s.get('http://httpbin.org/ip')
+
         """
         self._is_cache_disabled = True
         try:
@@ -115,15 +106,12 @@ class CachedSession(OriginalSession):
             self._is_cache_disabled = False
 
     def remove_expired_responses(self):
-        """Removes expired responses from storage"""
-        if not self._cache_expire_after:
-            return
-        self.cache.remove_old_entries(datetime.utcnow() - self._cache_expire_after)
+        """Remove expired responses from storage"""
+        self.cache.remove_expired_responses()
 
     def __repr__(self):
-        return "<CachedSession(%s('%s', ...), expire_after=%s, " "allowable_methods=%s)>" % (
-            self.cache.__class__.__name__,
-            self._cache_name,
-            self._cache_expire_after,
-            self._cache_allowable_methods,
+        return (
+            f'<CachedSession({self.cache.__class__.__name__}("{self._cache_name}", ...), '
+            f'expire_after={self._cache_expire_after}, '
+            f'allowable_methods={self._cache_allowable_methods})>'
         )
