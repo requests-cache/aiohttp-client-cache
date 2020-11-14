52beed5c22a362a64d63ebd523f2404fa801b9c5
Jordan Cook <jordan.cook@pioneer.com>
2020-11-14 17:14:54 -0600

    Handle request history; add created_at timestamp as CachedResponse attr instead of separate cache item

--- aiohttp_client_cache/__init__.py
+++ aiohttp_client_cache/__init__.py
@@ -1,7 +1,9 @@
-__version__ = '0.6'
+__version__ = '0.6.0'
 
 try:
     from aiohttp_client_cache.core import CachedSession
+    from aiohttp_client_cache.cached_response import CachedResponse
+    from aiohttp_client_cache.backends import *
 # When running setup.py outside a virtualenv
 except ImportError:
     pass
