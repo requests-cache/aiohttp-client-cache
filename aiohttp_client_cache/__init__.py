7ed51658acbcb0cc18cab61067b64acdb0705fa6
Jordan Cook <jordan.cook@pioneer.com>
2020-11-14 18:09:35 -0600

    Rename some more modules

--- aiohttp_client_cache/__init__.py
+++ aiohttp_client_cache/__init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.6.0'
 
 try:
-    from aiohttp_client_cache.core import CachedSession
-    from aiohttp_client_cache.cached_response import CachedResponse
+    from aiohttp_client_cache.session import CachedSession
+    from aiohttp_client_cache.response import CachedResponse
     from aiohttp_client_cache.backends import *
 # When running setup.py outside a virtualenv
 except ImportError:
