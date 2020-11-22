a854c2573015624d956422a0858e49d6fa29f524
Jordan Cook <jordan.cook@pioneer.com>
2020-11-21 20:17:54 -0600

    Rename some parameters; add some missing type annotations

--- aiohttp_client_cache/session.py
+++ aiohttp_client_cache/session.py
@@ -1,13 +1,13 @@
 """Core functions for cache configuration"""
 from contextlib import contextmanager
 from datetime import timedelta
-from typing import Callable, Union
+from typing import Callable, Iterable, Union
 
 import forge
 from aiohttp import ClientSession
 from aiohttp.typedefs import StrOrURL
 
-from aiohttp_client_cache.backends import create_backend
+from aiohttp_client_cache.backends import init_backend
 from aiohttp_client_cache.response import AnyResponse
 
 
@@ -19,11 +19,11 @@ class CachedSession(ClientSession):
         cache_name: str = 'http-cache',
         backend: str = None,
         expire_after: Union[int, timedelta] = None,
-        allowable_codes: tuple = (200,),
-        allowable_methods: tuple = ('GET',),
+        allowed_codes: tuple = (200,),
+        allowed_methods: tuple = ('GET', 'HEAD'),
         filter_fn: Callable = lambda r: True,
-        include_get_headers: bool = False,
-        ignored_parameters: bool = None,
+        include_headers: bool = False,
+        ignored_params: Iterable = None,
         **backend_options,
     ):
         """
@@ -37,10 +37,10 @@ class CachedSession(ClientSession):
             filter_fn: function that takes a :py:class:`aiohttp.ClientResponse` object and
                 returns a boolean indicating whether or not that response should be cached. Will be
                 applied to both new and previously cached responses
-            allowable_codes: Limit caching only for response with this codes
-            allowable_methods: Cache only requests of this methods
-            include_get_headers: Make response headers part of the cache key
-            ignored_parameters: List of request parameters to be excluded from the cache key.
+            allowed_codes: Limit caching only for response with this codes
+            allowed_methods: Cache only requests of this methods
+            include_headers: Make request headers part of the cache key
+            ignored_params: List of request parameters to be excluded from the cache key.
             backend_options: Additional backend-specific options; see :py:module:`.backends` for details
 
         The ``cache_name`` parameter will be used as follows depending on the backend:
@@ -54,45 +54,45 @@ class CachedSession(ClientSession):
         ``ignored_parameters`` to ignore specific request params. This is useful, for example, when
         requesting the same resource with different credentials or access tokens.
         """
-        self.cache = create_backend(
+        self.cache = init_backend(
             backend,
             cache_name,
-            expire_after,
+            expire_after=expire_after,
             filter_fn=filter_fn,
-            allowable_codes=allowable_codes,
-            allowable_methods=allowable_methods,
-            include_get_headers=include_get_headers,
-            ignored_parameters=ignored_parameters,
+            allowable_codes=allowed_codes,
+            allowable_methods=allowed_methods,
+            include_headers=include_headers,
+            ignored_params=ignored_params,
             **backend_options,
         )
         super().__init__()
 
     @forge.copy(ClientSession._request)
     async def _request(self, method: str, str_or_url: StrOrURL, **kwargs) -> AnyResponse:
-        """Wrapper around :py:func:`.SessionClient._request` that adds caching"""
+        """Wrapper around :py:meth:`.SessionClient._request` that adds caching"""
         cache_key = self.cache.create_key(method, str_or_url, **kwargs)
 
         # Attempt to fetch cached response; if missing or expired, fetch new one
         cached_response = await self.cache.get_response(cache_key)
-        if cached_response is None or getattr(cached_response, 'is_expired', False):
+        if cached_response and not getattr(cached_response, 'is_expired', False):
+            return cached_response
+        else:
             client_response = await super()._request(method, str_or_url, **kwargs)
             await client_response.read()
             await self.cache.save_response(cache_key, client_response)
             return client_response
-        else:
-            return cached_response
 
     @contextmanager
-    def cache_disabled(self):
-        """
-        Context manager for temporarily disabling cache
+    def disable_cache(self):
+        """Temporarily disable the cache
 
         Example:
 
-            >>> s = CachedSession()
-            >>> with s.cache_disabled():
-            ...     s.get('http://httpbin.org/ip')
-
+            >>> session = CachedSession()
+            >>> await session.get('http://httpbin.org/ip')
+            >>> with session.disable_cache():
+            >>>     # Will return a new response, not a cached one
+            >>>     await session.get('http://httpbin.org/ip')
         """
         self.cache.disabled = True
         yield
