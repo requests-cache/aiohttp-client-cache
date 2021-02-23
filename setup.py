f036bd8184f095c2b47ac04d902cf68041432bf1
Joseph Bergeron <jophish126@gmail.com>
2021-02-23 10:27:42 -0500

    Lint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extras_require = {
     # Packages used for CI jobs
     'build': ['coveralls', 'twine', 'wheel'],
     # Packages for all supported backends
-    'backends': ['aiosqlite', 'boto3', 'pymongo', 'redis'],
+    'backends': ['aiosqlite', 'boto3', 'motor', 'redis'],
     # Packages used for documentation builds
     'docs': [
         'm2r2',
