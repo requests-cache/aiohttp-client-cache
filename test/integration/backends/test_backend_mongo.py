550260dd35c49a898e53ef065ed6629575726d69
Jordan Cook <jordan.cook@pioneer.com>
2021-02-24 15:42:17 -0600

    Use pytest fixture to access db client instead of a test class

--- test/integration/backends/test_backend_mongo.py
+++ test/integration/backends/test_backend_mongo.py
@@ -8,7 +8,7 @@ from pymongo.errors import ConnectionFailure
 from aiohttp_client_cache.backends.mongo import MongoDBCache
 
 
-def test_connection():
+def is_db_running():
     """Test if a MongoDB server is running locally on the default port"""
     try:
         client = MongoClient(serverSelectionTimeoutMS=200)
@@ -18,137 +18,150 @@ def test_connection():
         return False
 
 
-pytestmark = pytest.mark.skipif(
-    not test_connection(), reason='MongoDB server required for integration tests'
-)
+pytestmark = [
+    pytest.mark.asyncio,
+    pytest.mark.skipif(not is_db_running(), reason='MongoDB server required for integration tests'),
+]
+
+db_name = 'aiohttp-cache'
+collection_name = 'responses'
 
 
-class TestMongoDBCache:
-    pytestmark = pytest.mark.asyncio
+@pytest.fixture(autouse=True, scope='function')
+async def cache_client(event_loop):
+    # We need to recreate the Motor client for every test method,
+    # else it will be using a different event loop than pytest.
+    connection = AsyncIOMotorClient()
+    cache_client = MongoDBCache(db_name, collection_name, connection)
 
-    db_name = 'aiohttp-cache'
-    collection_name = 'responses'
+    await connection.drop_database(db_name)
+    yield cache_client
+    await connection.drop_database(db_name)
 
-    def setup(self):
-        self.connection = AsyncIOMotorClient()
-        self.db = self.connection[self.db_name]
-        self.collection = self.db[self.collection_name]
-        self.cache_client = MongoDBCache(self.db_name, self.collection_name, self.connection)
 
-    @pytest.fixture(autouse=True)
-    async def drop_db(self, event_loop):
-        # We need to recreate the Motor client for every test method,
-        # else it will be using a different event loop than pytest.
-        self.setup()
-        await self.connection.drop_database(self.db_name)
-        yield
-        await self.connection.drop_database(self.db_name)
+async def test_clear(cache_client):
+    # Put some stuff in the DB
+    await cache_client.collection.insert_many({"x": i} for i in range(10))
 
-    async def test_clear(self):
-        # Put some stuff in the DB
-        await self.collection.insert_many({"x": i} for i in range(10))
+    # Validate that DB is non-empty
+    docs = await cache_client.collection.count_documents({})
+    assert docs == 10
 
-        # Validate that DB is non-empty
-        docs = await self.collection.count_documents({})
-        assert docs == 10
+    # Clear collection and validate that it's empty
+    await cache_client.clear()
 
-        # Clear collection and validate that it's empty
-        await self.cache_client.clear()
+    docs = await cache_client.collection.count_documents({})
+    assert docs == 0
 
-        docs = await self.collection.count_documents({})
-        assert docs == 0
 
-    async def test_contains_true(self):
-        _id = (await self.collection.insert_one({"test": "obj"})).inserted_id
-        result = await self.cache_client.contains(_id)
-        assert result is True
+async def test_contains_true(cache_client):
+    _id = (await cache_client.collection.insert_one({"test": "obj"})).inserted_id
+    result = await cache_client.contains(_id)
+    assert result is True
 
-    async def test_contains_false(self):
-        result = await self.cache_client.contains("some_id")
-        assert result is False
 
-    async def test_deletes_only_doc(self):
-        # Insert one doc and validate its existence
-        _id = (await self.collection.insert_one({"test": "obj"})).inserted_id
-        doc = await self.collection.find_one({"_id": _id})
-        assert doc
+async def test_contains_false(cache_client):
+    result = await cache_client.contains("some_id")
+    assert result is False
 
-        # Delete doc and validate its deletion
-        await self.cache_client.delete(_id)
-        doc = await self.collection.find_one({"_id": _id})
-        assert not doc
 
-    async def test_deletes_one_of_many(self):
-        # Insert a bunch of docs
-        inserted_ids = (await self.collection.insert_many({"x": i} for i in range(10))).inserted_ids
-        num_docs = await self.collection.count_documents({})
-        assert num_docs == 10
+async def test_deletes_only_doc(cache_client):
+    # Insert one doc and validate its existence
+    _id = (await cache_client.collection.insert_one({"test": "obj"})).inserted_id
+    doc = await cache_client.collection.find_one({"_id": _id})
+    assert doc
 
-        # Delete one of them
-        _id = inserted_ids[0]
-        await self.cache_client.delete(_id)
-        doc = await self.collection.find_one({"_id": _id})
-        assert not doc
+    # Delete doc and validate its deletion
+    await cache_client.delete(_id)
+    doc = await cache_client.collection.find_one({"_id": _id})
+    assert not doc
 
-        num_docs = await self.collection.count_documents({})
-        assert num_docs == 9
 
-    async def test_keys_many(self):
-        expected_ids = (await self.collection.insert_many({"x": i} for i in range(10))).inserted_ids
-        actual_ids = await self.cache_client.keys()
-        assert set(actual_ids) == set(expected_ids)
+async def test_deletes_one_of_many(cache_client):
+    # Insert a bunch of docs
+    inserted_ids = (
+        await cache_client.collection.insert_many({"x": i} for i in range(10))
+    ).inserted_ids
+    num_docs = await cache_client.collection.count_documents({})
+    assert num_docs == 10
 
-    async def test_keys_empty(self):
-        actual_ids = await self.cache_client.keys()
-        assert set(actual_ids) == set()
+    # Delete one of them
+    _id = inserted_ids[0]
+    await cache_client.delete(_id)
+    doc = await cache_client.collection.find_one({"_id": _id})
+    assert not doc
 
-    async def test_read_exists(self):
-        expected_data = "some_data"
-        _id = (await self.collection.insert_one({"data": expected_data})).inserted_id
-        actual_data = await self.cache_client.read(_id)
-        assert actual_data == expected_data
+    num_docs = await cache_client.collection.count_documents({})
+    assert num_docs == 9
 
-    async def test_read_does_not_exist(self):
-        fake_id = ObjectId("0" * 24)
-        actual_data = await self.cache_client.read(fake_id)
-        assert not actual_data
 
-    async def test_size_nonzero(self):
-        await self.collection.insert_many({"x": i} for i in range(10))
-        expected_size = 10
-        actual_size = await self.cache_client.size()
-        assert actual_size == expected_size
+async def test_keys_many(cache_client):
+    expected_ids = (
+        await cache_client.collection.insert_many({"x": i} for i in range(10))
+    ).inserted_ids
+    actual_ids = await cache_client.keys()
+    assert set(actual_ids) == set(expected_ids)
 
-    async def test_size_zero(self):
-        expected_size = 0
-        actual_size = await self.cache_client.size()
-        assert actual_size == expected_size
 
-    async def test_write_does_not_exist(self):
-        await self.cache_client.write("some_key", "some_data")
-        doc = await self.collection.find_one({"_id": "some_key"})
-        assert doc
-        assert doc["data"] == "some_data"
+async def test_keys_empty(cache_client):
+    actual_ids = await cache_client.keys()
+    assert set(actual_ids) == set()
 
-    async def test_write_does_exist(self):
-        _id = (
-            await self.collection.insert_one({"_id": "some_key", "data": "old_data"})
-        ).inserted_id
-        await self.cache_client.write(_id, "new_data")
 
-        doc = await self.collection.find_one({"_id": _id})
-        assert doc
-        assert doc["data"] == "new_data"
+async def test_read_exists(cache_client):
+    expected_data = "some_data"
+    _id = (await cache_client.collection.insert_one({"data": expected_data})).inserted_id
+    actual_data = await cache_client.read(_id)
+    assert actual_data == expected_data
 
-    async def test_values_none(self):
-        actual_results = await self.cache_client.values()
-        assert set(actual_results) == set()
 
-    async def test_values_many(self):
-        # If some entries are missing the "data" field for some reason, they
-        # should not be returned with the results.
-        await self.collection.insert_many({"data": i} for i in range(10))
-        await self.collection.insert_many({"not_data": i} for i in range(10))
-        actual_results = await self.cache_client.values()
-        expected_results = set(range(10))
-        assert set(actual_results) == expected_results
+async def test_read_does_not_exist(cache_client):
+    fake_id = ObjectId("0" * 24)
+    actual_data = await cache_client.read(fake_id)
+    assert not actual_data
+
+
+async def test_size_nonzero(cache_client):
+    await cache_client.collection.insert_many({"x": i} for i in range(10))
+    expected_size = 10
+    actual_size = await cache_client.size()
+    assert actual_size == expected_size
+
+
+async def test_size_zero(cache_client):
+    expected_size = 0
+    actual_size = await cache_client.size()
+    assert actual_size == expected_size
+
+
+async def test_write_does_not_exist(cache_client):
+    await cache_client.write("some_key", "some_data")
+    doc = await cache_client.collection.find_one({"_id": "some_key"})
+    assert doc
+    assert doc["data"] == "some_data"
+
+
+async def test_write_does_exist(cache_client):
+    _id = (
+        await cache_client.collection.insert_one({"_id": "some_key", "data": "old_data"})
+    ).inserted_id
+    await cache_client.write(_id, "new_data")
+
+    doc = await cache_client.collection.find_one({"_id": _id})
+    assert doc
+    assert doc["data"] == "new_data"
+
+
+async def test_values_none(cache_client):
+    actual_results = await cache_client.values()
+    assert set(actual_results) == set()
+
+
+async def test_values_many(cache_client):
+    # If some entries are missing the "data" field for some reason, they
+    # should not be returned with the results.
+    await cache_client.collection.insert_many({"data": i} for i in range(10))
+    await cache_client.collection.insert_many({"not_data": i} for i in range(10))
+    actual_results = await cache_client.values()
+    expected_results = set(range(10))
+    assert set(actual_results) == expected_results
