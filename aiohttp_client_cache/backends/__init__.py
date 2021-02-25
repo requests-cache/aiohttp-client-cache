6c42667df719fa6d32b703a6396758ce7781b89d
Jordan Cook <jordan.cook@pioneer.com>
2021-02-24 21:38:47 -0600

    Add better error messages: If backend dependencies are missing, capture original ImportError and delay raising until class is initialized

--- aiohttp_client_cache/backends/__init__.py
+++ aiohttp_client_cache/backends/__init__.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from aiohttp_client_cache.backends.base import (  # noqa: F401
     BaseCache,
     CacheBackend,
@@ -5,24 +7,40 @@ from aiohttp_client_cache.backends.base import (  # noqa: F401
     ResponseOrKey,
 )
 
+logger = getLogger(__name__)
+
+
+def get_placeholder_backend(original_exception):
+    """This creates a placeholder type for a backend class that does not have dependencies
+    installed. This allows delaying the ImportError until init is called, rather then when imported.
+    """
+
+    class PlaceholderBackend:
+        def __init__(*args, **kwargs):
+            logger.error('Dependencies are not installed for this backend')
+            raise original_exception
+
+    return PlaceholderBackend
+
+
 # Import all backends for which dependencies are installed
 try:
     from aiohttp_client_cache.backends.dynamodb import DynamoDBBackend
-except ImportError:
-    DynamoDBBackend = None  # type: ignore
+except ImportError as e:
+    DynamoDBBackend = get_placeholder_backend(e)  # type: ignore
 try:
     from aiohttp_client_cache.backends.gridfs import GridFSBackend
-except ImportError:
-    GridFSBackend = None  # type: ignore
+except ImportError as e:
+    GridFSBackend = get_placeholder_backend(e)  # type: ignore
 try:
     from aiohttp_client_cache.backends.mongo import MongoDBBackend
-except ImportError:
-    MongoDBBackend = None  # type: ignore
+except ImportError as e:
+    MongoDBBackend = get_placeholder_backend(e)  # type: ignore
 try:
     from aiohttp_client_cache.backends.redis import RedisBackend
-except ImportError:
-    RedisBackend = None  # type: ignore
+except ImportError as e:
+    RedisBackend = get_placeholder_backend(e)  # type: ignore
 try:
     from aiohttp_client_cache.backends.sqlite import SQLiteBackend
-except ImportError:
-    SQLiteBackend = None  # type: ignore
+except ImportError as e:
+    SQLiteBackend = get_placeholder_backend(e)  # type: ignore
