442d3ebe87789d514a7d67c57713a70465e93e8e
Jordan Cook <jordan.cook@pioneer.com>
2023-09-19 13:25:03 -0500

    Remove hiding ImportErrors in top-level init (use case for this is no longer relevant)

--- aiohttp_client_cache/__init__.py
+++ aiohttp_client_cache/__init__.py
@@ -1,10 +1,6 @@
 __version__ = '0.8.3'
 
 # flake8: noqa: F401, F403
-try:
-    from aiohttp_client_cache.backends import *
-    from aiohttp_client_cache.response import CachedResponse
-    from aiohttp_client_cache.session import CachedSession
-# When running setup.py outside a virtualenv
-except ImportError:
-    pass
+from aiohttp_client_cache.backends import *
+from aiohttp_client_cache.response import CachedResponse
+from aiohttp_client_cache.session import CachedSession
