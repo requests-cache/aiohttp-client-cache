21a12f67a6033d4da0f70e4a51f0c544de551bad
Thomas Neidhart <thomas.neidhart@gmail.com>
2023-10-05 21:42:42 +0200

    Improve github example.

--- examples/github.py
+++ examples/github.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # fmt: off
 """
-An example of making conditional request to the GitHub Rest API`
+An example of making conditional requests to the GitHub Rest API`
 """
+
 import asyncio
 import logging
 
@@ -12,12 +13,15 @@ CACHE_DIR = "~/.cache/aiohttp-requests"
 
 
 async def main():
-    cache = FileBackend(cache_name=CACHE_DIR)
+    cache = FileBackend(cache_name=CACHE_DIR, use_temp=True)
     await cache.clear()
 
     org = "requests-cache"
     url = f"https://api.github.com/orgs/{org}/repos"
 
+    # we make 2 requests for the same resource (list of all repos of the requests-cache organization)
+    # the second request refreshes the cached response with the remote server
+    # the debug output should illustrate that the cached response gets refreshed
     async with CachedSession(cache=cache) as session:
         response = await session.get(url)
         print(f"url = {response.url}, status = {response.status}, "
@@ -31,5 +35,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("aiohttp_client_cache").setLevel(logging.DEBUG)
     asyncio.run(main())
